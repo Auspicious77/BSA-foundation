cf6c6309093cd84005f61c1441ed671f53afc4a5
auspicious <elishaibukun@gmail.com>
2022-06-21 12:51:47 +0100

    worked on homescreen

--- BSAfoundation/Screens/Auth/AuthStack.js
+++ BSAfoundation/Screens/Auth/AuthStack.js
@@ -14,7 +14,7 @@ const Stack = createStackNavigator();
 export default function AuthStack() {
   return (
     <Stack.Navigator>
-<Stack.Screen
+    <Stack.Screen
         name="Index"
         component={Login}
         options={{
